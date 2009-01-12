c60b6ed50bd79d79fcc2a9c9ab4f2865b8efd45e
Luke Palmer <lrpalmer@gmail.com>
2009-01-12 05:29:06 -0700

    Changed to a tuple-indexing form.  Is this useful?

--- HeteroMap/Map.hs
+++ HeteroMap/Map.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE IncoherentInstances, RankNTypes, TypeFamilies, MultiParamTypeClasses, FlexibleInstances #-}
+{-# LANGUAGE IncoherentInstances, RankNTypes, MultiParamTypeClasses, FlexibleInstances #-}
 
 module HeteroMap.Map
     ( Key, newKey
-    , Map, empty, singleton, insert, lookup, overwrite, union
+    , Map, empty, singleton, insert, lookup, overwrite
     )
 where
 
@@ -14,46 +14,46 @@ import Unsafe.Coerce (unsafeCoerce)
 import System.IO.Unsafe (unsafePerformIO)
 import Data.Maybe (fromJust)
 
-class In x xs
-instance In x (x,xs)
-instance In x xs => In x (y,xs)
+class In x xs where 
+    access :: xs -> x
+    replace :: x -> xs -> xs
+instance In x (x :* xs) where 
+    access (a:*as) = a
+    replace x (a:*as) = (x:*as)
+instance In x xs => In x (y:*xs) where
+    access (a:*as) = access as
+    replace x (a:*as) = (a:*replace x as)
+
+data Z = Z
+data a :* b = a :* !b
 
 -- | A Key in a heterogeneous map.  @x@ is the key identifier type, 
 -- which ensures that we don't look up a key in a map that doesn't
 -- have it.
-newtype Key x a = Key Unique
+data Key x a where
+    Key :: Key a a
 
 -- | A heterogeneous map, including keys @xs@.
-newtype Map xs = Map (Map.Map Unique Any)
+newtype Map xs = Map xs
 
-empty :: Map ()
-empty = Map Map.empty
+empty :: Map Z
+empty = Map Z
 
-singleton :: Key x a -> a -> Map (x,())
-singleton k v = insert k v empty
+singleton :: Key x a -> a -> Map (x :* Z)
+singleton Key v = Map (v :* Z)
 
 -- | Allocate a new key, which is only valid within the passed function
 -- (and will be a type error if it tries to escape).
 newKey :: (forall x. Key x a -> b) -> b
-newKey cc = cc (Key key)
-    where
-    {-# NOINLINE key #-}
-    key = unsafePerformIO newUnique
+newKey cc = cc Key
 
-insert :: Key x a -> a -> Map xs -> Map (x,xs)
-insert (Key key) val (Map m) = Map (Map.insert key (unsafeCoerce val) m)
+insert :: Key x a -> a -> Map xs -> Map (x :* xs)
+insert Key val (Map m) = Map (val :* m)
 
 lookup :: In x xs => Key x a -> Map xs -> a
-lookup (Key refid) (Map m) = unsafeCoerce . fromJust $ Map.lookup refid m
+lookup Key (Map m) = access m
 
 -- | Same as insert, except does not add the key identifier to the map,
 -- instead ensuring that it's already there.
 overwrite :: In x xs => Key x a -> a -> Map xs -> Map xs
-overwrite (Key refid) x (Map m) = Map (Map.insert refid (unsafeCoerce x) m)
-
-type family Union xs ys
-type instance Union () ys = ys
-type instance Union (x,xs) ys = (x, Union xs ys)
-
-union :: Map xs -> Map ys -> Map (Union xs ys)
-union (Map m) (Map m') = Map (Map.union m m')
+overwrite Key x (Map m) = Map (replace x m)
